67cf45e63f761d32b0f2828b632c96ae9e7bbc33
Joshua Halstead <jhalstead85@gmail.com>
2011-10-02 16:56:57 -0500

    Renamed Piece enum to PieceType and removed typdef declaration from the Piece struct.

--- Chess/misc.h
+++ Chess/misc.h
@@ -4,15 +4,15 @@
 // Defines several miscellaneous classes used by the program
 
 enum Color {WHITE=0, BLACK=1};
-enum Piece {NOPIECE=0, PAWN=1, KNIGHT=-3, BISHOP=3, ROOK=5, QUEEN=9, KING=1000};
+enum PieceType {NOPIECE=0, PAWN=1, KNIGHT=-3, BISHOP=3, ROOK=5, QUEEN=9, KING=1000};
 
-typedef struct {
+struct Piece {
 	Color pieceColor;
-	Piece pieceType;
-} Piece;
+	PieceType pieceType;
+};
 
-typedef struct {
+struct Player {
 	Color playerColor;
-} Player;
+};
 
 #endif
