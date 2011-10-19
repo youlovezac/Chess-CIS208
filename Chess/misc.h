3e0626c53e5fa72e05f3252281c4969bbd61c2e9
Joshua Halstead <Your product service request information>
2011-10-19 18:23:53 -0500

    Testing to see if I can commit from a different computer.

--- Chess/misc.h
+++ Chess/misc.h
@@ -2,6 +2,7 @@
 #define MISC_H
 
 // Defines several miscellaneous classes used by the program
+//
 
 enum Color {WHITE=0, BLACK=1};
 enum PieceType {NOPIECE=0, PAWN=1, KNIGHT=-3, BISHOP=3, ROOK=5, QUEEN=9, KING=1000};
