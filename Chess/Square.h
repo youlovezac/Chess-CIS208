5e6d438fe53af628bad99ef53b7334c4154f7ed4
Alex Shaw <alex_shaw@comcast.net>
2011-10-18 21:44:01 -0500

    the endif in Square.h doesn't need anything after it

--- Chess/Square.h
+++ Chess/Square.h
@@ -3,14 +3,13 @@
 
 #include "misc.h"
 
-
 class Square {
 public:
 	Square(); // Default constructor
 	Square(const Square& s); // Copy constructor
 	Square& operator=(const Square& s); // Overloaded assignment operator
 	bool operator==(const Square& s); // Equivalance operator
-    bool operator!=(const Square& s); // Inequivalence operator
+	bool operator!=(const Square& s); // Inequivalence operator
 	Color getColor() const;
 	Piece getPiece() const;
 	int getRow() const;
@@ -25,4 +24,4 @@ private:
 	Piece currPiece;
 };
 
-#endif SQUARE_H
+#endif
